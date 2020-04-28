af0026bfae753aaffce0244f9d30489e692d3baa
jtoledoiq <jtoledoiq@gmail.com>
2020-04-28 19:38:57 +0200

    create file react-camera.js

---
+++ components/react-camera.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import { render } from 'react-dom';
+import { RNCamera } from 'react-native-camera';
+
+export default class ReactCamera extends React.Component {
+    render() {
+
+        return (
+            <View>
+                <Text>Cafsfafasfasf</Text>
+            </View>
+            );
+    }
+}
