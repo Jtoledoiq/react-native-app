dc0a7fc2a4137d63ac1f9de4c0c243f10d4a0b47
jtoledoiq <jtoledoiq@gmail.com>
2020-05-08 13:19:23 +0200

    add some features to the signup page

--- components/signup.js
+++ components/signup.js
@@ -8,9 +8,25 @@ export default class SignUp extends React.Component {
         this.state = {
             name: '',
             email: '',
-            password: ''
+            password: '',
+            confirmPassword:'',
         }
     }
+
+    validateEmail(mail) {
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if(this.state.email.length==0) {
+            return <View></View>
+        }
+        else if (reg.test(this.state.email.trim()) === false) {
+            return <View><Text>Introduce a correct email</Text></View>
+        } else {
+            return <View></View>
+        }
+        
+    }
+
     render() {
 
 
@@ -22,33 +38,42 @@ export default class SignUp extends React.Component {
                 <View style={styles.body}>
                     <View><Text>Sign up</Text></View>
                     <View style={{alignItems:'center', justifyContent:'center'}}>
-                        <View style={{borderWidth:1, borderRadius:50, flexDirection:'row', justifyContent:'center', alignItems:'center', width:250, height:50, margin:5}}>
+                        <View style={{borderWidth:1, borderRadius:50, flexDirection:'row', justifyContent:'center', alignItems:'center', width:270, height:50, margin:5}}>
                             <Image source={require('../img/person.png')} style={{height:30, width:30, marginLeft:20}}/>
                             <TextInput
-                            style={{ width:200, fontSize:18, marginLeft:10}}
+                            style={{ width:210, fontSize:15, marginLeft:10}}
                             onChangeText={textInputValue =>this.setState({name: textInputValue})}
                             placeholder="Name"
                             />
                         </View>
 
-                        <View style={{borderWidth:1, borderRadius:50, flexDirection:'row', justifyContent:'center', alignItems:'center', width:250, height:50, margin:5}}>
+                        <View style={{borderWidth:1, borderRadius:50, flexDirection:'row', justifyContent:'center', alignItems:'center', width:270, height:50, margin:5}}>
                             <Image source={require('../img/email.png')} style={{height:30, width:30, marginLeft:20}}/>
                             <TextInput
-                            style={{ width:200, fontSize:18, marginLeft:10}}
+                            style={{ width:210, fontSize:15, marginLeft:10}}
                             onChangeText={textInputValue =>this.setState({email: textInputValue})}
                             placeholder="E-mail"
                             />
                         </View>
-
-                        <View style={{borderWidth:1, borderRadius:50, flexDirection:'row', justifyContent:'center', alignItems:'center', width:250, height:50, margin:5}}>
+                        {this.validateEmail(this.state.email)}
+                        <View style={{borderWidth:1, borderRadius:50, flexDirection:'row', justifyContent:'center', alignItems:'center', width:270, height:50, margin:5}}>
                             <Image source={require('../img/key.png')} style={{height:30, width:30, marginLeft:20}}/>
                             <TextInput
-                            style={{ width:200, fontSize:18, marginLeft:10}}
+                            style={{ width:210, fontSize:15, marginLeft:10}}
                             onChangeText={textInputValue =>this.setState({password: textInputValue})}
                             placeholder="Password"
                             />
                         </View>
 
+                        <View style={{borderWidth:1, borderRadius:50, flexDirection:'row', justifyContent:'center', alignItems:'center', width:270, height:50, margin:5}}>
+                            <Image source={require('../img/key.png')} style={{height:30, width:30, marginLeft:20}}/>
+                            <TextInput
+                            style={{ width:210, fontSize:15, marginLeft:10}}
+                            onChangeText={textInputValue =>this.setState({confirmPassword: textInputValue})}
+                            placeholder="Confirm Password"
+                            />
+                        </View>
+                        {this.state.password == this.state.confirmPassword ? <View></View> : <Text>The password doesn't match</Text>}    
                         <TouchableOpacity 
                             style={{width:180, height:50, backgroundColor:'lightblue', borderRadius:50, alignItems:'center', justifyContent:'center', marginTop:20}}
                             onPress={()=>console.log(this.state.name)}
